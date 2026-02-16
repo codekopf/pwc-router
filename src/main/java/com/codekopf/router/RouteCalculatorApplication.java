a1480dde94c93569846e5803ef0003d82cf4ed70
Andrej Buday <andrej.buday@gmail.com>
2026-02-16 10:25:57 +0100

    Added basic SpringBoot files

---
+++ src/main/java/com/codekopf/router/RouteCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.codekopf.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codekopf/router/RouteCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codekopf.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
