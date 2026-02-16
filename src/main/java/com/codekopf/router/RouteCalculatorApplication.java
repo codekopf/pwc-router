52390914901f2d910b2f30ba3a9a0b274fa48b51
Andrej Buday <andrej.buday@gmail.com>
2026-02-17 00:23:45 +0100

    Removed public on main method - not required in Java25

--- src/main/java/com/codekopf/router/RouteCalculatorApplication.java
+++ src/main/java/com/codekopf/router/RouteCalculatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RouteCalculatorApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(RouteCalculatorApplication.class, args);
 	}
 
